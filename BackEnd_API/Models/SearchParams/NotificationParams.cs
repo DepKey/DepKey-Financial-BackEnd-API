3fbe076d9642a16ea2354f0c39de4765a04b091b
DepKeyUser <DepkeyUser@DK102.DEPKEY.local>
2018-08-30 15:18:52 +0200

    ignore .vs

---
+++ BackEnd_API/Models/SearchParams/NotificationParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_API.Models.SearchParams
+{
+    public class NotificationParams
+    {
+        public int? ID { get; set; }
+        public string Description { get; set; }
+        public bool? IsDeleted { get; set; }
+        public bool? IsSeen { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int? RelatedID { get; set; }
+
+    }
+}
